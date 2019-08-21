90dfb701fa0136ce94f3375d2a5e5e2853b89254
Nicolas Gagnon <Nicolas.z.gagnon@gmail.com>
2019-08-21 20:46:29 +0100

    added todos

---
+++ src/API.py
@@ -0,0 +1,15 @@
+# Written by Nicolas Gagnon 2019
+
+#todo list
+# - GET /search/{username} Searches the data for the specific username
+# - GET /people Returns all people with pagination
+# - DELETE /people/{username} Delete a person
+
+def findUser(user_id):
+  pass
+
+def getAllUsers():
+  pass
+
+def deleteUser(user_id):
+  pass
